514058d91452fce73b536049d51ffc4e5e8ee2d8
eltoncampos1 <eltoncampos36@gmail.com>
2025-11-26 13:06:44 -0300

    formatting

--- main.go
+++ main.go
@@ -8,55 +8,54 @@ import (
 	"sync/atomic"
 )
 
-type  Backend struct {
-	URL *url.URL
-	Alive bool
-	mux sync.RWMutex
+type Backend struct {
+	URL          *url.URL
+	Alive        bool
+	mux          sync.RWMutex
 	ReverseProxy *httputil.ReverseProxy
 }
 
-type ServerPool struct{
-	 backends []*Backend
-	 current uint64
- }
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
 
-func main(){
- u, _ :=  url.Parse("http://localhost:8080")
- rp := httputil.NewSingleHostReverseProxy(u)
- http.HandleFunc("/", rp.ServeHTTP)
- http.ListenAndServe(":8080", nil)
+func main() {
+	u, _ := url.Parse("http://localhost:8080")
+	rp := httputil.NewSingleHostReverseProxy(u)
+	http.HandleFunc("/", rp.ServeHTTP)
+	http.ListenAndServe(":8080", nil)
 }
 
 func (b *Backend) SetAlive(alive bool) {
- b.mux.Lock()
- b.Alive = alive
- b.mux.Unlock()
+	b.mux.Lock()
+	b.Alive = alive
+	b.mux.Unlock()
 }
 
 func (b *Backend) IsAlive() (alive bool) {
- b.mux.RLock()
- alive = b.Alive
- b.mux.RUnlock()
- return
+	b.mux.RLock()
+	alive = b.Alive
+	b.mux.RUnlock()
+	return
 }
 
 func (s *ServerPool) NextIndex() int {
 	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
 }
 
-
 func (s *ServerPool) GetNextPerr() *Backend {
 	next := s.NextIndex()
 	l := len(s.backends) + next
-	
+
 	for i := range l {
 		idx := i % len(s.backends)
 		if s.backends[idx].IsAlive() {
-		if i != next {
-			atomic.StoreUint64(&s.current, uint64(idx))
+			if i != next {
+				atomic.StoreUint64(&s.current, uint64(idx))
+			}
+			return s.backends[idx]
 		}
-		}
-	   return s.backends[idx]
 	}
-	return  nil
+	return nil
 }
