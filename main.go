098a2f24d819d8d556e24b3cdc9a838f329df961
eltoncampos1 <eltoncampos36@gmail.com>
2025-11-26 13:05:24 -0300

    choosing backend process and recover active backend

---
+++ main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+	"sync"
+	"sync/atomic"
+)
+
+type  Backend struct {
+	URL *url.URL
+	Alive bool
+	mux sync.RWMutex
+	ReverseProxy *httputil.ReverseProxy
+}
+
+type ServerPool struct{
+	 backends []*Backend
+	 current uint64
+ }
+
+func main(){
+ u, _ :=  url.Parse("http://localhost:8080")
+ rp := httputil.NewSingleHostReverseProxy(u)
+ http.HandleFunc("/", rp.ServeHTTP)
+ http.ListenAndServe(":8080", nil)
+}
+
+func (b *Backend) SetAlive(alive bool) {
+ b.mux.Lock()
+ b.Alive = alive
+ b.mux.Unlock()
+}
+
+func (b *Backend) IsAlive() (alive bool) {
+ b.mux.RLock()
+ alive = b.Alive
+ b.mux.RUnlock()
+ return
+}
+
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+
+func (s *ServerPool) GetNextPerr() *Backend {
+	next := s.NextIndex()
+	l := len(s.backends) + next
+	
+	for i := range l {
+		idx := i % len(s.backends)
+		if s.backends[idx].IsAlive() {
+		if i != next {
+			atomic.StoreUint64(&s.current, uint64(idx))
+		}
+		}
+	   return s.backends[idx]
+	}
+	return  nil
+}
